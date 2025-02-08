56c34ce5a2719842347f3bc08f54bd09d744bd16
teyebnine <teyebnine30@gmail.com>
2025-02-08 02:41:05 +0000

    verigier

--- src/main/java/iscae/master/sb/dao/entities/GarageEntity.java
+++ src/main/java/iscae/master/sb/dao/entities/GarageEntity.java
@@ -1,35 +1,35 @@
-package iscae.master.sb.dao.entities;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name = "garage", schema = "public", catalog = "activite1")
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class GarageEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-
-    @Column(name = "nom", length = 100, nullable = false)
-    private String nom;
-
-    @Column(name = "address", length = 200)
-    private String address;
-
-    @Column(name = "telephone", length = 20)
-    private String telephone;
-
-    @Column(name = "email", length = 100)
-    private String email;
-
-    @Column(name = "capacite")
-    private Integer capacite;
-}
+//package iscae.master.sb.dao.entities;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table(name = "garage", schema = "public", catalog = "activite1")
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class GarageEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    private Long id;
+//
+//    @Column(name = "nom", length = 100, nullable = false)
+//    private String nom;
+//
+//    @Column(name = "address", length = 200)
+//    private String address;
+//
+//    @Column(name = "telephone", length = 20)
+//    private String telephone;
+//
+//    @Column(name = "email", length = 100)
+//    private String email;
+//
+//    @Column(name = "capacite")
+//    private Integer capacite;
+//}
